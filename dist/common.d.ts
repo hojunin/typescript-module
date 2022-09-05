bcbaf85b09f2e91ac722577bd8d9000c446d1bf4
hojunin <dlsghwns@naver.com>
2022-09-05 10:51:39 +0900

    PrePublish Commit

---
+++ dist/common.d.ts
@@ -0,0 +1,5 @@
+declare type ID = string;
+declare type URL = string;
+export type { ID, URL };
+declare type TEST = string;
+export default TEST;
---
+++ lib/common.ts
@@ -0,0 +1,8 @@
+type ID = string;
+type URL = string;
+
+export type { ID, URL };
+
+type TEST = string;
+
+export default TEST;
